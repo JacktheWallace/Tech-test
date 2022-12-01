b049176582dbf6c265d260e39f0bb4e8c7e32cc0
Jack Wallace <jackwillamwallace@hotmail.com>
2022-12-01 12:07:13 +0000

    first commit, I've done a lot already but in my eagerness to get as much done as possible for got to actually push my work up to git hub

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
+
+describe("App", () => {
+    const { asFragment } = render (<App />);
+    it("renders correctly", () => {
+        expect(asFragment()).toMatchSnapshot();
+    });
+    });
---
+++ src/components/SearchResults.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/SearchResults.css";
+
+const SearchResults =  ({ results }) => {
+    if (!results.length) {
+    return <p>No Results!</p>;
+    } else {
+    return (
+        <>
+            {results.map((image) => (
+                <img className="card-image" src={image} alt="spaceImage" />
+            ))}
+        </>
+    );
+    }
+};
+
+export default SearchResults;
